a314267873cadeaaece242b7662b4a72b2491c2c
Walker Gray <55608093+wakfi@users.noreply.github.com>
2020-10-02 11:01:34 -0700

    Initial Commit

---
+++ moreSwiftFun/main.swift
@@ -0,0 +1,12 @@
+//
+//  main.swift
+//  moreSwiftFun
+//
+//  Created by Gray, John Walker on 10/2/20.
+//  Copyright © 2020 Walker Gray. All rights reserved.
+//
+
+import Foundation
+
+print("Hello, World!")
+
